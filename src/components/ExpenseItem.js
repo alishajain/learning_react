d74345ffcdf030e402c2ebbb6cead7fdb42863d5
alishajain <jain.alisha14@gmail.com>
2022-04-30 16:40:04 +0530

    Added styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
-function Expenseitem() {
-  return <h2>Expense Item!</h2>
+import "./ExpenseItem.css"
+function ExpenseItem(props) {
+  return (<div className="expense-item">
+            <div>{props.date.toISOString()}</div>
+            <div className="expense-item__description">
+              <h2>{props.title}</h2>
+              <div className="expense-item__price">${props.amount}</div>
+            </div>
+  </div>
+  );
 }
 
-export default Expenseitem;
+export default ExpenseItem;
